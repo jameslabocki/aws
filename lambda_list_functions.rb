5854c81a7320d197e90004dc0fcf91c4eb8f2372
James Labocki <jlabocki@redhat.com>
2015-12-11 16:00:46 -0500

    changes

--- lambda_list_functions.rb
+++ lambda_list_functions.rb
@@ -1,15 +1,47 @@
 #!/usr/bin/ruby
 
 require 'aws-sdk-core'
+require 'configparser'
 
-lambda = Aws::Lambda::Client.new(
-  region: 'us-west-2',
-  access_key_id: 'AKIAISX3CVPZYUSDM3TQ',
-  secret_access_key: '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'
-)
 
-#print lambda.operation_names
+class Lambda
 
-myfunctions = lambda.list_functions
+  def configure 
+    cp = ConfigParser.new('config/aws.cfg')
+    @region = cp['config']['region']
+    @accessKeyId = cp['config']['access_key_id']
+    @secretAccessKey = cp['config']['secret_access_key']
+  end
 
-print myfunctions[1]
+  def authenticate
+    myregion = @region
+    myaccessKeyId = @accessKeyId
+    mysecretAccessKey = @secretAccessKey
+    @mylambda = Aws::Lambda::Client.new(
+      region: 'us-west-2',
+      access_key_id: 'AKIAJP2YIATIX4CZDEAQ',
+      secret_access_key: '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'
+    )
+  end
+
+  def listfunctions
+    myfunctions = @mylambda.list_functions
+    puts myfunctions[1]
+  end
+
+end
+
+
+#Create a new object
+object = Lambda. new
+
+#Get configuration information
+object.configure
+
+#Authenticate
+#object.authenticate('us-west-2','AKIAJP2YIATIX4CZDEAQ','MbKnVoLvon8sAV77Q50+fgcj823UzltRt3rPx8xi')
+object.authenticate
+
+#List Functions
+object.listfunctions
+
